66b721e5f0168ad403535742f1cf166e71da2af0
Aleksandr Grigorii <domosedov.dev@gmail.com>
2021-03-16 09:10:41 +0300

    feat: add new tailwindcss jit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,15 @@ const Home: FC = () => {
   }
 
   return (
-    <div className='bg-red-100 sm:bg-red-300 tablet:bg-red-500 laptop:bg-red-700 container max-w-screen-laptop'>
+    <div className='container max-w-screen-lg bg-yellow-100 md:'>
       <h1>Home Page</h1>
+      <div className='w-[200px] lg:w-[500px]'>
+        Далеко-далеко, за словесными горами в стране гласных и согласных живут
+        рыбные тексты. Языком, ручеек большой пояс рот выйти несколько текст
+        осталось за над заглавных страна родного повстречался, знаках текстов
+        решила. Семь, языкового?
+      </div>
+      <div className='bg-green-500 w-[200px] h-[200px]' />
       <div className='flex items-stretch space-x-4'>
         <div className='flex-1'>
           <div className='aspect-w-1 aspect-h-1 md:aspect-w-16 md:aspect-h-9'>
@@ -49,7 +56,7 @@ const Home: FC = () => {
           <div className='flex space-x-2 mt-4'>
             <div className='w-1/2 md:line-clamp-2'>
               Далеко-далеко за словесными горами в стране гласных и согласных
-              живут рыбные тексты. Реторический свой семь буквоград но текст
+              живут рыбные тексты. Риторический свой семь буквоград но текст
               переписали переписывается составитель даже заглавных последний.
               Свой это проектах рукопись злых прямо, всеми предупреждал одна
               свое оксмокс вскоре необходимыми использовало строчка возвращайся
